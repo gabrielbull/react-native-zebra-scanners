96ad68cb96c14a08e0f044f7e8649249e12d314b
Gabriel Bull <me@gabrielbull.com>
2018-03-30 21:42:21 -0400

    Added target

---
+++ ios/RCTZebraScanners/RCTZebraScanners.h
@@ -0,0 +1,12 @@
+//
+//  RCTZebraScanners.h
+//  RCTZebraScanners
+//
+//  Created by Gabriel Bull on 18-03-30.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTZebraScanners : NSObject
+
+@end
---
+++ ios/RCTZebraScanners/RCTZebraScanners.m
@@ -0,0 +1,12 @@
+//
+//  RCTZebraScanners.m
+//  RCTZebraScanners
+//
+//  Created by Gabriel Bull on 18-03-30.
+//
+
+#import "RCTZebraScanners.h"
+
+@implementation RCTZebraScanners
+
+@end
